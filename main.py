a12c1af869070fceca64a388b3196843d840cfcb
Arpit Khandelwal <57443699+rockstr99@users.noreply.github.com>
2020-12-09 17:30:04 +0530

    View bot works fine, looking forward to make it .exe file

---
+++ main.py
@@ -0,0 +1,24 @@
+from selenium import webdriver
+import time
+from random import randrange
+
+LINK = input("Enter your youtube video link here: ")
+TABS = int(input("Enter number of tabs (**warning this depends upon your system, it is not advisable to open more than 4 tabs**) : "))
+TIME = int(input("Enter view time in seconds: "))
+
+browser_list = []
+
+for i in range(0, TABS):
+    browser = webdriver.Chrome("./Driver//chromedriver.exe")
+    browser_list.append(browser)    
+
+
+for browser in browser_list : 
+    browser.get (LINK)
+
+while(True):
+    for i in range(len(browser_list)):
+        time.sleep(TIME)
+        browser_list[i].refresh()
+        print("Browser number: ", i, "has been refreshed !")
+        
